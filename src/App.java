d2a6349e8babfec7ca3288d099b7063f01960f09
Devdat <pdevdat2020@gmail.com>
2025-11-05 20:00:52 +0530

    Displaying messages functionality created

--- src/App.java
+++ src/App.java
@@ -10,10 +10,31 @@ public class App {
     private static String SELECT_QUERY = "SELECT * FROM messages WHERE id > 0 ORDER BY id ASC";
     public static int id = 0;
 
+    private static final String[] COLORS = {
+            "\u001B[31m", // Red
+            "\u001B[32m", // Green
+            "\u001B[33m", // Yellow
+            "\u001B[34m", // Blue
+            "\u001B[35m", // Magenta
+            "\u001B[36m", // Cyan
+            "\u001B[91m", // Bright Red
+            "\u001B[92m", // Bright Green
+            "\u001B[93m", // Bright Yellow
+            "\u001B[94m", // Bright Blue
+            "\u001B[95m", // Bright Magenta
+            "\u001B[96m" // Bright Cyan
+    };
+
+    public static String getRandomColor() {
+        Random rand = new Random();
+        return COLORS[rand.nextInt(COLORS.length)];
+    }
+
     public static void main(String[] args) throws InterruptedException {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter username: ");
         sc.nextLine();
+        String ucolor = getRandomColor();
 
         try (Connection conn = DBConnection.getConnection();
                 Statement stmt = conn.createStatement();
--- src/message.java
+++ src/message.java
@@ -23,24 +23,12 @@ public class message {
         // this.timestamp = timestamp;
     }
 
-    private String getAnsiColor(String hexColor) {
-        return switch (hexColor.toUpperCase()) {
-            case "#FF0000" -> "\u001B[31m"; // red
-            case "#00FF00" -> "\u001B[32m"; // green
-            case "#0000FF" -> "\u001B[34m"; // blue
-            case "#FF5733" -> "\u001B[91m"; // orange/red
-            case "#33C1FF" -> "\u001B[36m"; // cyan
-            case "#FFC300" -> "\u001B[33m"; // yellow
-            default -> "\u001B[37m"; // white fallback
-        };
-    }
-
     public static final String BOLD = "\u001B[1m";
     public static final String RESET = "\u001B[0m";
 
     public void fetchmessage() {
         String time = timestamp.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
-        String output = "[" + time + "] " + BOLD + getAnsiColor(colorCode) + username + RESET + " : " + messageText;
+        String output = "[" + time + "] " + BOLD + colorCode + username + RESET + " : " + messageText;
         System.out.println(output);
     }
 
