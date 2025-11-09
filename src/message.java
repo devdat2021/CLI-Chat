81c466390b6e63349a53d14da8c3861e42897ff6
Devdat <pdevdat2020@gmail.com>
2025-11-09 15:44:37 +0530

    Shifted UTC time to IST

--- src/message.java
+++ src/message.java
@@ -28,7 +28,9 @@ public class message {
         synchronized (System.out) {
             System.out.print("\r"); // move to start of current line
             System.out.print("\033[K"); // clear line
-            String time = timestamp.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+            // String time = timestamp.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+            LocalDateTime localTime = timestamp.plusHours(5).plusMinutes(30); // shifting UTC time to IST
+            String time = localTime.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
 
             if (!((colorCode.equals(App.ucolor)) && (username.equals(App.uname)))) {
                 String output = "[" + time + "] " + BOLD + colorCode + username + RESET + " : " + messageText;
